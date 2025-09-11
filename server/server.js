0e4b266b55cabc611d19a504272f0020b74dbede
matthewhu8 <Hu.Mat@northeastern.edu>
2025-09-10 21:08:46 -0400

    good work buzz

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.post('/api/test-post', (req, res) => {
 
 // Handle React routing, return all requests to React app in production
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 }
